d320d3f10f6c67ef9082afebb9efb0e0c124d06d
DETUMO <adetumo@gmail.com>
2022-10-02 17:42:30 +0100

    java

---
+++ main.js
@@ -0,0 +1,9 @@
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primarynav = document.querySelector(".primary-navigation");
+
+navToggle.addEventListener('click', () => {
+    primarynav.hasAttribute('data-visible') 
+    ? navToggle.setAttribute("aria-expanded", false) 
+    : navToggle.setAttribute("aria-expanded", true);  
+    primarynav.toggleAttribute("data-visible");
+});
